ec3d8ed734c43e601618b4dad17f3651a06079f7
cartersm <cartersm@rose-hulman.edu>
2015-11-11 15:20:01 -0500

    Fixed firebase and keep-screen-awake

--- Android/MercuryController/app/src/main/java/edu/rosehulman/mercury/mercurycontroller/MainActivity.java
+++ Android/MercuryController/app/src/main/java/edu/rosehulman/mercury/mercurycontroller/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.rosehulman.mercury.mercurycontroller;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.firebase.client.AuthData;
@@ -9,9 +10,9 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
-import edu.rosehulman.me435.RobotActivity;
+import edu.rosehulman.me435.AccessoryActivity;
 
-public class MainActivity extends RobotActivity {
+public class MainActivity extends AccessoryActivity {
 
     public static final String EMAIL = "cartersm@rose-hulman.edu";
     public static final String PASSWORD = "RoseHulmanMercury2016";
@@ -29,21 +30,55 @@ public class MainActivity extends RobotActivity {
 
         mFirebaseRef = new Firebase("https://mercury-robotics-16.firebaseio.com/");
         if (mFirebaseRef.getAuth() == null) {
-            mFirebaseRef.authWithPassword(EMAIL, PASSWORD,
-                    new Firebase.AuthResultHandler() {
-                        @Override
-                        public void onAuthenticated(AuthData authData) {
-                            Toast.makeText(MainActivity.this, "login successful", Toast.LENGTH_SHORT).show();
-                        }
-
-                        @Override
-                        public void onAuthenticationError(FirebaseError firebaseError) {
-                            Toast.makeText(MainActivity.this, firebaseError.getMessage(), Toast.LENGTH_LONG).show();
-
-                        }
-                    });
+            auth();
+        } else if (mFirebaseRef.getAuth().getExpires() < mBirthTime / 1000) {
+            Log.w("MERCC", "Auth expired, reauthenticating...");
+            mFirebaseRef.unauth();
+            auth();
         } else {
-            mFirebaseRef.child("ledCommands").addChildEventListener(new LedCommandListener());
+            Toast.makeText(MainActivity.this, "Already logged in", Toast.LENGTH_SHORT).show();
+            Log.d("MERCC", "Already logged in");
+        }
+        mFirebaseRef.child("ledCommands").addChildEventListener(new LedCommandListener());
+    }
+
+    private void auth() {
+        mFirebaseRef.authWithPassword(EMAIL, PASSWORD,
+                new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        Toast.makeText(MainActivity.this, "login successful", Toast.LENGTH_SHORT).show();
+                        Log.d("MERCC", "login succeeded");
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        Toast.makeText(MainActivity.this, firebaseError.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.e("MERCC", "login failed: " + firebaseError.getMessage());
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseRef.unauth();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFirebaseRef.unauth();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mFirebaseRef.getAuth() == null) {
+            auth();
+        } else {
+            mFirebaseRef.unauth();
+            auth();
         }
     }
 
@@ -57,10 +92,12 @@ public class MainActivity extends RobotActivity {
 
             if (timestamp < mBirthTime) {
                 // ignore commands sent before we started up
+                Log.d("MERCC", "Ignoring old command");
                 return;
             }
             String command = String.format(LED_FORMAT_STRING, ledNumber, status.toUpperCase());
             Toast.makeText(MainActivity.this, "Sending command \"" + command + "\"", Toast.LENGTH_SHORT).show();
+            Log.d("MERCC", "Sending command \"" + command + "\"");
             sendCommand(command);
         }
 
@@ -82,6 +119,7 @@ public class MainActivity extends RobotActivity {
         @Override
         public void onCancelled(FirebaseError firebaseError) {
             // unused
+            Log.e("MERCC", firebaseError.getMessage());
         }
     }
 }
