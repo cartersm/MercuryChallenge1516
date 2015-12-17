4f67ba0d12501eb6777e1533fdb873caf54781a1
cartersm <cartersm@rose-hulman.edu>
2015-12-16 21:00:47 -0500

    Fixed Arduino code for USB Accessory

--- Arduino/MercuryController/MercuryController.ino
+++ Arduino/MercuryController/MercuryController.ino
@@ -7,8 +7,8 @@
 #define PIN_LED_4 67
 #define PIN_LED_5 68
 #define PIN_LED_6 69
-#define ON 1
-#define OFF 0
+#define LED_ON 1
+#define LED_OFF 0
 
 char manufacturer[] = "Rose-Hulman.mercury";
 char model[] = "Controller";
@@ -17,8 +17,12 @@ char versionStr[] = "1.0";
 char onMessage[] = "I have an idea!";
 char offMessage[] = "Nope. Lost it.";
 
-AndroidAccessory acc(manufacturer, model, "Controlling the Robot",
-                     versionStr, "https://sites.google.com/site/me435spring2013", "12345");
+AndroidAccessory acc(manufacturer, 
+                     model, 
+                     "Controlling the Robot",
+                     versionStr, 
+                     "https://sites.google.com/site/me435spring2015", 
+                     "12345");
 
 char rxBuf[255];
 void setup() {
@@ -41,13 +45,14 @@ void loop() {
     if (len > 0) {
       rxBuf[len - 1] = '\0';
       String inputString = String(rxBuf);
+      Serial.println("Received Command: " + inputString);
       if (inputString.startsWith("LED")) {
         int startIndex = inputString.indexOf(" ") + 1;
         int endIndex = inputString.indexOf(" ", startIndex);
         String ledNumberStr = inputString.substring(startIndex, endIndex);
         int ledNumber = ledNumberStr.toInt();
         String stateStr = inputString.substring(endIndex + 1);
-        int state = (stateStr.equals("ON")) ? ON : OFF;
+        int state = (stateStr.equals("ON")) ? LED_ON : LED_OFF;
         switch (ledNumber) {
         case 1:
           digitalWrite(PIN_LED_1, state);
