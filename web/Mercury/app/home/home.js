96ce97717b4a264e312efaaa833bf6b4df2f97c1
cartersm <cartersm@rose-hulman.edu>
2016-02-07 15:22:20 -0500

    change cm->in, remove distance restrictions

--- web/Mercury/app/home/home.js
+++ web/Mercury/app/home/home.js
@@ -84,7 +84,7 @@ angular.module('Mercury.home', [
 
             $scope.sendMotorCommand = function (cmd) {
                 console.log('sending motor command (' +
-                    cmd.distance + 'cm, ' +
+                    cmd.distance + 'in, ' +
                     cmd.angle + 'deg, ' +
                     cmd.serpentine + ')');
                 firebase.child('motorCommands')
